94b4fcf919beb78dc04c1b16367ad5115417c4b4
eden <ontheedge101@gmail.com>
2024-07-09 15:35:21 +0300

    First push

--- Client_Side.py
+++ Client_Side.py
@@ -2,25 +2,61 @@ import socket
 import threading
 import struct
 
+servers_list = [("127.0.0.1", 9999), ("127.0.0.1", 9998), ("127.0.0.1", 9997), ("127.0.0.1", 9996), ("127.0.0.1", 9995)]
+header_format = '>bbhh'
+
+
+# 0 type 1byte , 1 subtype 1byte, 2 length 2bytes, 3 sublen 2bytes
+
 
 def client_setup():
-    server_ip = input("Server ip: ")
-    server_port = input("Server Port: ")
-    server_address = (server_ip, server_port)
-    print(server_ip, server_port)
+    server = int(input("Choose a server to connect to (0-4): "))
+    server_address = (servers_list[server])
+
     try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.connect(server_address)
-        client_socket, client_address = server_socket.accept()
-        return server_socket, client_socket
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_sock.connect(server_address)
+        address = server_address[0] + ':' + str(server_address[1])
+        server_sock.send(address.encode())
+        return server_sock
 
     except ConnectionRefusedError:
         print('Connection refused on address')
 
-def client_loop():
-    server_socket, client_socket = client_setup()
+
+def create_user():
+    user_name = input("Enter your username: ")
+    user_name = user_name.encode()
+    server_socket.send(struct.pack(header_format, 2, 1, len(user_name), 0) + user_name)
+    threading.Thread(target=handle_massage).start()
+    send_massage()
 
 
-def protocol_handler(client_socket, header):
-    print(header)
+def handle_massage():
+    while True:
+        try:
+            data = server_socket.recv(1024)
+            print("\nreceived massage: ")
+            sender, massage, receiver = data.decode().split('\0')
+            print(f'from: {sender}\n{massage} \nto: {receiver}')
+            if not data:
+                break
+        except Exception as e:
+            print(e)
+
+
+def send_massage():
+    while True:
+        receiver, massage = input().split(" ", 1)
+        massage += '\0'
+        massage += receiver
+        massage = massage.encode()
+        if massage:
+            server_socket.send(struct.pack(header_format, 3, 0, len(massage), len(receiver)) + massage)
+
+
+server_socket = client_setup()
+if server_socket:
+    create_user()
+
--- Server_Side.py
+++ Server_Side.py
@@ -3,31 +3,67 @@ import struct
 from icecream import ic
 import threading
 
-
 header_format = '>bbhh'
-server_dict = {}
+# 0 type 1byte , 1 subtype 1byte, 2 length 2bytes, 3 sublen 2bytes
 
 
-def server_setup(address):
+def server_setup():
+    # set up the listen socket
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_sock.bind(address)
+    server_sock.bind(server_address)
     server_sock.listen(5)
+    print(f'Server Socket Listening on {listen_address}')
+    # add the server to the server_dict
+    server_dict[listen_address] = server_sock
+    ic(server_dict)
+
+
+def server_accept_loop(server_sock):
+    while True:
+        peer_socket, add = server_sock.accept()
+        print(f'Accepted connection from {add}')
+        listen_add = peer_socket.recv(14)
+        listen_add = listen_add.decode()
+        if listen_add != listen_address:
+            server_dict[listen_add] = peer_socket
+        ic(server_dict)
+        client_handler = threading.Thread(target=handle_connection, args=(peer_socket,))
+        client_handler.start()
+
+
+def connect_to_peer(ip, port):
+    try:
+        other_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        other_server.connect((ip, port))
+        print(f'Connected to {ip}:{port}')
+        other_server.send(listen_address.encode())
+        other_server.send(struct.pack(header_format, 2, 0, 0, 0))
+        server_dict[ip + ":" + str(port)] = other_server
+        ic(server_dict)
+        return other_server
+    except ConnectionRefusedError:
+        print(f'Failed to connect to {server_ip}:{port}')
+    except Exception as e:
+        print(f'Failed {e}')
+
+
+def test_servers_connection():
     for port in ports:
-        try:
-            peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            peer_socket.connect((address, port))
-            peer_socket.send(struct.pack(header_format, 0, 0, 0, 0))
-            handle_connection(server_sock)
-        except ConnectionRefusedError as e:
-            print(f"Connection refused on port: {port}")
-    return server_sock
+        sock = connect_to_peer(server_ip, port)
+        if sock:
+            threading.Thread(target=handle_connection, args=(sock,)).start()
+            sock.send(struct.pack(header_format, 0, 0, 0, 0))
+            break
 
 
-def connect_to_others(server_info):
-    print(server_info)
-    for address, sock in server_info.items():
-        sock.connect(address)
+def connect_from_list(servers_list):
+    print(servers_list)
+    for address in servers_list:
+        if address not in server_dict.keys():
+            ip, port = address.split(":")
+            sock = connect_to_peer(ip, int(port))
+            threading.Thread(target=handle_connection, args=(sock,)).start()
 
 
 def handle_connection(listen_socket):
@@ -43,95 +79,113 @@ def handle_connection(listen_socket):
             print(f"Error: {e}")
 
 
-def dict_manager(case_id, dic=None, msg=None, server_info=None):
-    match case_id:
-        case 0:
-            # server info to send
-            address = dic.items()
-            massage = b''
-            for ip, port in server_address:
-                massage += ip + ":" + port + '\0'
-            print(massage)
-            return massage
-        case 1:
-            # clients info to send
-            names = dic.keys()
-            massage = b''
-            for name in names:
-                massage += name + '\0'
-            return massage
-        case 2:
-            # server info receive and connect
-            massage = msg.split('\0').decode()
-            connect_to_others(massage)
-        case 3:
-            # client info receive and update
-            massage = msg.split('\0').decode()
-            for name in massage:
-                clients_dict[name] = server_info
-        case _:
-            print("unknown type")
-
-
 def protocol_handler(client_socket, header):
+    def check_client():
+        pass
+
     match header[0]:
         case 0:
+            # info request
             if header[1] == 0:
                 # info on server request
-                client_socket.send(struct.pack(header_format, 1, 0, len(server_dict), 0))
-                package = dict_manager(header[1], server_dict)
-                client_socket.send(package)
+                massage = ''
+                addresses = server_dict.keys()
+                ic(addresses)
+                for address in addresses:
+                    massage += address + '\0'
+                package = massage.encode()
+                ic(package)
+                client_socket.send(struct.pack(header_format, 1, 0, len(package), 0) + package)
+                # client_socket.send(package)
+
             elif header[1] == 1:
                 # info on clients request
-                package = dict_manager(header[1], clients_dict)
-                client_socket.send(package)
+                massage = ''
+                names = clients_dict.keys()
+                for name in names:
+                    massage += name + '\0'
+                package = massage.encode()
+                client_socket.send(struct.pack(header_format, 1, 1, len(package), 0) + package)
             else:
                 print("unexpected protocol header")
 
         case 1:
+            # info receive
             if header[1] == 0:
                 # receive server info
                 length = header[2]
-                client_socket.recv(length)
-                dict_manager(2, msg=client_socket.recv(length))
+                data = client_socket.recv(length)
+                # server connect
+                massage = data.decode()
+                massage = massage.split('\0')
+                massage.remove("")
+                ic(massage)
+                connect_from_list(massage)
                 print(header)
             elif header[1] == 1:
                 # receive client info
                 length = header[2]
-                dict_manager(3, msg=client_socket.recv(length), server_info=server_dict[client_socket])
-                print(clients_dict)
+                data = client_socket.recv(length)
+                print(data)
+            else:
+                print("unexpected protocol header")
 
         case 2:
             if header[1] == 0:
                 # become a server
-                server_dict[server_socket] = server_address
+                ic(client_socket)
                 print(header, "server")
             elif header[1] == 1:
                 # become a client
-                length = header[2]
-                client_name = client_socket.recv(length)
-                clients_dict[client_name] = server_dict[server_socket]
+                client_name = client_socket.recv(header[2]).decode()
+                clients_dict[client_name] = client_socket
+                ic(clients_dict)
+            else:
+                print("unexpected protocol header")
 
         case 3:
             # send the message
-            if client_socket == server_socket:
-                data = client_socket.recv(1024)
-                for sock in server_dict:
-                    sock.send(struct.pack(header_format, 1, 0, len(server_dict), 0))
-                    sock.send(data)
-            else:
-                data = client_socket.recv(1024)
-                print(data.decode())
-            print(header, "massage")
+            if client_socket in clients_dict.values():
+                for name, sock in clients_dict.items():
+                    if sock == client_socket:
+                        sender = name
+                        ic(sender)
+                        data = client_socket.recv(header[2]).decode()
+                        massage, receiver = data.split('\0')
+                        ic(massage, receiver)
+                        massage = sender + "\0" + massage + '\0' + receiver
+                        if receiver in clients_dict.keys():
+                            receiver_socket = clients_dict[receiver]
+                            ic(receiver_socket)
+                            receiver_socket.send(massage.encode())
+                            break
+                        else:
+                            for server in server_dict.values():
+
+                                if server != server_socket:
+                                    server.send(struct.pack(header_format, 3, 0, len(massage), len(receiver)) + massage.encode())
+            if client_socket in server_dict.values():
+                data = client_socket.recv(header[2] + header[3]).decode()
+                sender, massage, receiver = data.split('\0')
+                if receiver in clients_dict.keys():
+                    clients_dict[receiver].send(data.encode())
 
         case _:
             print("unknown header")
 
 
 server_ip = "127.0.0.1"
-ports = [9999, 9998, 9997, 9996, 9995, 9994, 9993, 9992, 9991, 9990]
-print("ports: \n", ports)
-server_port = int(input("Server port: "))
-server_address = (server_ip, server_port)
-server_socket = server_setup(server_address)
+ports = [9999, 9998, 9997, 9996, 9995]
 clients_dict = {}
+server_dict = {}
+print("ports: \n", ports)
+server_port = ports[int(input("Server port (0 - 4): "))]
+ports.remove(server_port)
+server_address = (server_ip, server_port)
+listen_address = server_address[0] + ":" + str(server_port)
+server_setup()
+server_socket = server_dict[listen_address]
+threading.Thread(target=server_accept_loop, args=(server_socket,)).start()
+test_servers_connection()
+
+
